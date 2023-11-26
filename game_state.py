53c5eac3e9b952430aaef0cdb1f93a3cb0fff4c9
muneebmoin2000 <52342199+muneebmoin2000@users.noreply.github.com>
2023-11-25 23:04:08 -0500

    changed data structure for easier implementation

--- game_state.py
+++ game_state.py
@@ -1,8 +1,29 @@
-class Board():
-    def __init__(self):
+from numpy import ndarray
 
-        self.player1 = 0b000000000
-        self.player2 = 0b000000000
+class Board():
+    """Board class made to store the current game state of the tictactoe board; 
+    search and evaluation classes will be stored in seperate files"""    
     
-    def visualize_board(self,P1,P2):
-        pass
+    def __init__(self):
+        """The constructor will initialize the board with empty values
+        The entire game state of the board is being stored as a python list."""
+        self.board = ([
+            ["","",""],
+            ["","",""],
+            ["","",""]
+        ])
+        
+    def visualize_board(self,boards):
+        """This function is useful for displaying the game onto the console for now;
+        In future implementations, this console centric display will be replaced with a GUI"""
+
+        print()
+        print(f" {boards[0][0]} | {boards[0][1]} | {boards[0][2]} ")
+        print("---+---+---")
+        print(f" {boards[1][0]} | {boards[1][1]} | {boards[1][2]} ")
+        print("---+---+---")
+        print(f" {boards[2][0]} | {boards[2][1]} | {boards[2][2]} ")
+        print()
+
+b = Board()
+b.visualize_board(b.board)
