1a2ff6382af168f14f06f21d6f11af8396f65690
muneebmoin2000 <52342199+muneebmoin2000@users.noreply.github.com>
2023-12-17 00:40:23 -0500

    " Finishing the models.py file"

--- game_state.py
+++ game_state.py
@@ -13,11 +13,13 @@ class Board():
             [" "," "," "],
             [" "," "," "]
         ])
-        self.players = ["X","O"]
-        self.curr_player = random.choice(self.players)
+        self.teams = ["X","O"]
+        self.curr_player = random.choice(["Computer","Myself"])
         self.winner = None
         self.end_states = ["X Wins", "O Wins", "Tied Game","Please Continue Game","Please Start the Game"]
         self.used_spots = 0
+        self.available_moves = []
+
 
     def visualize(self,boards):
         """This function is useful for displaying the game onto the console for now;
@@ -31,9 +33,12 @@ class Board():
         print(f" {boards[2][0]} | {boards[2][1]} | {boards[2][2]} ")
         print()
 
-    def check_winner(self):
+    def check_state(self):
         """If there are spots still available,
          winner could still be determined based on the moves """
+        
+        if self.used_spots == 0:
+            return self.end_states[4]
         
         for i in range(3):
             #horizontal
@@ -48,18 +53,32 @@ class Board():
         elif self.board[0][2] == self.board[1][1] == self.board[2][0]:
             self.winner = self.board[0][2]
         
-        return self.winner
-        
-    def curr_state(self):
-        """check the final end-state of the game and return it
-        Its possible that the game never started in which is also an edge case"""
-        if self.used_spots == 0:
-            return self.end_states[4]
         if self.winner is None and self.used_spots < 9:
             return self.end_states[3]
         if self.winner is None and self.used_spots == 9:
             return self.end_states[2]
-        if self.winner == self.players[1]:
+        if self.winner == self.teams[1]:
             return self.end_states[1]
-        if self.winner == self.players[0]:
+        if self.winner == self.teams[0]:
             return self.end_states[0]
+        
+    # def ending_state(self):
+    #     """check the final end-state of the game and return it
+    #     Its possible that the game never started in which is also an edge case"""
+    def toggle_player(self):
+        if self.curr_player == self.teams[0]:
+            self.curr_player = self.teams[1]
+        else:
+            self.curr_player = self.teams[0]
+    
+    def move_finder(self):
+
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == " ":
+                    self.available_moves.append([i,j])
+                else:
+                    continue
+
+    def play_game(self):
+        [i,j] = (random.choice([0,1,2]),random.choice([0,1,2]))
